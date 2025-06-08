f9d0bd936ac24c80922986691ad67ce33d61dfa7
VibeCade <admin@vibecade.ai>
2025-06-07 23:32:04 -0400

    Fix Terser Build Error

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
     target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
